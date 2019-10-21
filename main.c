3787a6543b3f40deaf06d6e70db41bf6e7af32d4
udesc <>
2019-10-21 14:10:17 -0300

    teste de print e scanf

--- main.c
+++ main.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct data {
+typedef struct data {
     int dia,mes,ano;
-};
+}data;
 
-struct telefone {
-    int codInt;
-    int codArea;
-    int numero;
-};
+typedef struct telefone {
+    char codInternacional[3];
+    char codArea[3];
+    char numero[9];
+}telefone;
 
-struct endereco {
+typedef struct endereco {
     char rua[200];
     int numero;
     char complemento[100];
     char bairro[100];
     int cep[100];
     char cidade[100];
-};
+}endereco;
 
-struct pessoa {
+typedef struct pessoa {
     char nome[100];
     char email[100];
-    struct endereco ende;
-    struct telefone tele;
-    struct data nascimento;
+    endereco ende;
+    telefone tele;
+    data nascimento;
     char obs[500];
-};
+}pessoa;
 
 int main(){
 
+    pessoa pessoas[100];
 
-    printf("cu\n");
+    // scanf("%[A-Z a-z]s", pessoas[0].nome);
+    // printf("%s\n", pessoas[0].nome);
 
     return 0;
 }
